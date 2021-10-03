69c6000c455510c664b690a7608a2923e902228e
Nick <nick14@gmail.com>
2021-10-03 13:51:43 -0700

    Added file system dialog to macos desktop

--- lib/menu/dev_menu.dart
+++ lib/menu/dev_menu.dart
@@ -1,9 +1,13 @@
 import 'package:OpenDeadlock/menu/menu_title_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart' show kIsWeb;
+import 'dart:io' show Platform;
+import 'package:file_selector/file_selector.dart';
 
 class DevMenu extends StatelessWidget {
   DevMenu({Key? key, required this.title}) : super(key: key);
   final String title;
+  String? directory;
 
   @override
   Widget build(BuildContext context) {
@@ -24,15 +28,47 @@ class DevMenu extends StatelessWidget {
                     Column(children: [
                       Column(
                         children: [
+                          Row(
+                            children: [
+                              Column(
+                                children: [
+                                  Text("Platform: ")
+                                ]
+                              ),
+                              SizedBox(width: 20),
+                              Column(
+                                children: [
+                                  Text(getPlatformType())
+                                ]
+                              )
+                            ]
+                          ),
+                          SizedBox(height: 20),
                           ElevatedButton(
                             onPressed: () {
-                              print("foo");
+                              pickDeadlockDirectory();
                             },
                             style: ElevatedButton.styleFrom(
                                 minimumSize: Size(125, 35)),
                             child: Text("Find Deadlock"),
                           ),
                           SizedBox(height: 5),
+                          Row(
+                              children: [
+                                Column(
+                                    children: [
+                                      Text("Directory: ")
+                                    ]
+                                ),
+                                SizedBox(width: 20),
+                                Column(
+                                    children: [
+                                      Text(directory ?? "(none)")
+                                    ]
+                                )
+                              ]
+                          ),
+                          SizedBox(height: 20),
                           ElevatedButton(
                             onPressed: null,
                             style: ElevatedButton.styleFrom(
@@ -66,5 +102,25 @@ class DevMenu extends StatelessWidget {
         ),
       )),
     );
+  }
+
+  String getPlatformType() {
+    if (kIsWeb) {
+      return "Web";
+    } else if (Platform.isAndroid || Platform.isIOS) {
+      return "Mobile";
+    } else if (Platform.isLinux || Platform.isMacOS || Platform.isWindows) {
+      return "Desktop";
+    }
+    return "Unknown";
+  }
+
+  void pickDeadlockDirectory() {
+    getDirectoryPath(
+      confirmButtonText: "Choose",
+    ).then((value) {
+      directory = value;
+      // TODO refresh display
+    });
   }
 }
