ccb467d98c5479aed8c64fd8bbd88aebdbc2162e
Nick Matelli <nick14@gmail.com>
2023-10-19 16:00:05 -0700

    Updated devmenu to display selected directory

--- lib/menu/dev_menu.dart
+++ lib/menu/dev_menu.dart
@@ -7,120 +7,131 @@ import 'package:file_selector/file_selector.dart';
 class DevMenu extends StatefulWidget {
   DevMenu({Key? key, required this.title}) : super(key: key);
   final String title;
-  String? directory;
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-          child: DefaultTextStyle(
-        style: TextStyle(color: Colors.black),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Card(
-                color: Colors.grey,
-                child: Column(children: [
-                  MenuTitleBar(title: title),
-                  SizedBox(height: 20),
-                  Row(mainAxisSize: MainAxisSize.min, children: [
-                    SizedBox(width: 20),
-                    Column(children: [
-                      Column(
-                        children: [
-                          Row(
+  State<DevMenu> createState() => _DevMenuState();
+}
+
+class _DevMenuState extends State<DevMenu> {
+    String? directory;
+    String title = "foo";
+
+    @override
+    Widget build(BuildContext context) {
+        return Scaffold(
+            body: Center(
+                child: DefaultTextStyle(
+            style: TextStyle(color: Colors.black),
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                Card(
+                    color: Colors.grey,
+                    child: Column(children: [
+                        MenuTitleBar(title: title),
+                        SizedBox(height: 20),
+                        Row(mainAxisSize: MainAxisSize.min, children: [
+                        SizedBox(width: 20),
+                        Column(children: [
+                            Column(
                             children: [
-                              Column(
+                                Row(
                                 children: [
-                                  Text("Platform: ")
-                                ]
-                              ),
-                              SizedBox(width: 20),
-                              Column(
-                                children: [
-                                  Text(getPlatformType())
-                                ]
-                              )
-                            ]
-                          ),
-                          SizedBox(height: 20),
-                          ElevatedButton(
-                            onPressed: () {
-                              pickDeadlockDirectory();
-                            },
-                            style: ElevatedButton.styleFrom(
-                                minimumSize: Size(125, 35)),
-                            child: Text("Find Deadlock"),
-                          ),
-                          SizedBox(height: 5),
-                          Row(
-                              children: [
-                                Column(
+                                    Column(
                                     children: [
-                                      Text("Directory: ")
+                                        Text("Platform: ")
+                                    ]
+                                    ),
+                                    SizedBox(width: 20),
+                                    Column(
+                                    children: [
+                                        Text(getPlatformType())
+                                    ]
+                                    )
+                                ]
+                                ),
+                                SizedBox(height: 20),
+                                ElevatedButton(
+                                onPressed: pickDeadlockDirectory,
+                                style: ElevatedButton.styleFrom(
+                                    minimumSize: Size(125, 35)),
+                                child: Text("Find Deadlock"),
+                                ),
+                                SizedBox(height: 5),
+                                Row(
+                                    children: [
+                                    Column(
+                                        children: [
+                                            Text("Directory: ")
+                                        ]
+                                    ),
+                                    SizedBox(width: 20),
+                                    Column(
+                                        children: [
+                                            Text(directory ?? "(none)")
+                                        ]
+                                    )
                                     ]
                                 ),
-                                SizedBox(width: 20),
-                                Column(
-                                    children: [
-                                      Text(directory ?? "(none)")
-                                    ]
-                                )
-                              ]
-                          ),
-                          SizedBox(height: 20),
-                          ElevatedButton(
-                            onPressed: null,
-                            style: ElevatedButton.styleFrom(
-                                minimumSize: Size(125, 35)),
-                            child: Text("Run Decoder"),
-                          ),
-                          SizedBox(height: 10),
-                          ElevatedButton(
-                            onPressed: null,
-                            style: ElevatedButton.styleFrom(
-                                minimumSize: Size(125, 35)),
-                            child: const Text("View Comic"),
-                          ),
-                          SizedBox(height: 10),
-                          ElevatedButton(
-                            onPressed: () {
-                              Navigator.pop(context);
-                            },
-                            style: ElevatedButton.styleFrom(
-                                minimumSize: Size(125, 35)),
-                            child: Text("Back"),
-                          ),
-                        ],
-                      ),
-                    ]),
-                    SizedBox(width: 20),
-                  ]),
-                  SizedBox(height: 20),
-                ]))
-          ],
-        ),
-      )),
-    );
-  }
+                                SizedBox(height: 20),
+                                ElevatedButton(
+                                onPressed: (directory == null ? null : runDecoder),
+                                style: ElevatedButton.styleFrom(
+                                    minimumSize: Size(125, 35)),
+                                child: Text("Run Decoder"),
+                                ),
+                                SizedBox(height: 10),
+                                ElevatedButton(
+                                onPressed: null,
+                                style: ElevatedButton.styleFrom(
+                                    minimumSize: Size(125, 35)),
+                                child: const Text("View Comic"),
+                                ),
+                                SizedBox(height: 10),
+                                ElevatedButton(
+                                onPressed: () {
+                                    Navigator.pop(context);
+                                },
+                                style: ElevatedButton.styleFrom(
+                                    minimumSize: Size(125, 35)),
+                                child: Text("Back"),
+                                ),
+                            ],
+                            ),
+                        ]),
+                        SizedBox(width: 20),
+                        ]),
+                        SizedBox(height: 20),
+                    ]))
+                ],
+            ),
+            )),
+        );
+    }
 
-  String getPlatformType() {
+    String getPlatformType() {
     if (kIsWeb) {
-      return "Web";
+        return "Web";
     } else if (Platform.isAndroid || Platform.isIOS) {
-      return "Mobile";
+        return "Mobile";
     } else if (Platform.isLinux || Platform.isMacOS || Platform.isWindows) {
-      return "Desktop";
+        return "Desktop";
     }
     return "Unknown";
-  }
+    }
 
-  void pickDeadlockDirectory() {
-    getDirectoryPath(
-      confirmButtonText: "Choose",
-    ).then((value) {
-      directory = value;
-      // TODO refresh display
-    });
-  }
+    void pickDeadlockDirectory() {
+        getDirectoryPath(
+            confirmButtonText: "Choose",
+        ).then((value) {
+            setState(() {
+                directory = value;
+            });
+        });
+    }
+
+    void runDecoder() {
+        print("BAZ");
+    }
+
 }
